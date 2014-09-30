9cbeb58b5efc2658c66068d799e276da4abffa18
Dan Beckett <dan.beckett@door4.com>
2014-09-30 17:14:06 +0100

    Removed video assets from repo, altered how these are called and got new ACF export to reflect these changes

--- section-type-video.php
+++ section-type-video.php
@@ -1,6 +1,6 @@
 <video autoplay loop muted class="fp-video">
-	<source src="<?php echo get_template_directory_uri(); ?>/examples/imgs/<?php the_field('video_filename'); ?>.mp4" type="video/mp4">
-	<source src="<?php echo get_template_directory_uri(); ?>/examples/imgs/<?php the_field('video_filename'); ?>.webm" type="video/webm">
+	<source src="<?php the_field('video_media_mp4'); ?>" type="video/mp4">
+	<source src="<?php the_field('video_media_webm'); ?>" type="video/webm">
 </video>
 <?php if(get_field('video_overlay_text')) { ?>
 	<div class="layer">
